6986db40f15e1d39deb900a6e17c951b2c24ec8c
nnalin04 <nnalin04@gmail.com>
2020-08-31 01:18:58 +0530

    Added code and test to Calculate Multiple Ride

--- src/main/java/InvoiceGenerator.java
+++ src/main/java/InvoiceGenerator.java
@@ -2,8 +2,18 @@ public class InvoiceGenerator {
 
     private static final int COST_PER_TIME = 1;
     private static final double MIN_COST_PER_KM = 10;
+    private static final double MIN_FARE = 5;
 
     public double calculateFare(double distance, int time) {
-        return distance * MIN_COST_PER_KM + time * COST_PER_TIME;
+        double totalFare = distance * MIN_COST_PER_KM + time * COST_PER_TIME;
+        return Math.max(totalFare, MIN_FARE);
+    }
+
+    public double calculateFare(Ride[] rides) {
+        double totalFare = 0;
+        for (Ride ride : rides ){
+            totalFare += this.calculateFare(ride.distance, ride.time);
+        }
+        return totalFare;
     }
 }
